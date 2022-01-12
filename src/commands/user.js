c0917112231ec3e311079e0e2eed455b72ecde37
Cleberson Saller <contato@csaller.com>
2022-01-11 21:29:04 -0300

    fix: eslint warnings

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,10 +1,9 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
 module.exports = {
+  data: new SlashCommandBuilder().setName('user').setDescription('Replies with user info'),
 
-	data: new SlashCommandBuilder().setName('user').setDescription('Replies with user info'),
-	
-    async execute(interaction) {
-		await interaction.reply(`User name: ${interaction.user.tag}\nUser ID: ${interaction.user.id}\n Your avatar: ${interaction.user.displayAvatarURL({ format: 'png', dynamic: true })}`);
-	},
-};
+  async execute (interaction) {
+    await interaction.reply(`User name: ${interaction.user.tag}\nUser ID: ${interaction.user.id}\n Your avatar: ${interaction.user.displayAvatarURL({ format: 'png', dynamic: true })}`)
+  }
+}
