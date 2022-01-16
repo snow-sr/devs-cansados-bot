04af0bfae941ed54122dbd8b7fb04199a3a1d086
Bessa <silvaisraelb@gmail.com>
2022-01-16 01:10:45 -0300

    refatoração - organização

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ const { Client, Intents, Collection } = require('discord.js')
 const { botToken, guildId } = require('./config') // TODO: change the guildId to DevsCansados Id
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'))
 const buffer = []
 
 client.commands = new Collection()
 
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'))
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  client.commands.set(command.data.name, command)
-}
-
-client.once('ready', async () => {
-  console.log('Ready!')
-  const filter = message => message.content.includes('discord') // TODO: create message filter
-  const guild = await client.guilds.fetch(guildId)
+function setCommands(guild) {
+  const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'))
   let commands;
+
   if (guild) {
     commands = guild.commands
   } else {
@@ -28,30 +20,47 @@ client.once('ready', async () => {
 
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
+    client.commands.set(command.data.name, command)
     commands.create(command.data)
   }
+}
+
+function setSpamCollector(channel) {
+  const spamCollector = channel.createMessageCollector({ filter: message => !message.author.bot })
+  spamCollector.on('collect', message => {
+    if (buffer.length >= 1000) {
+      buffer.pop()
+    }
+    if (buffer.includes(message.content)) {
+      message.channel.send('PARA DE SPAMMAR OW :face_with_symbols_over_mouth::face_with_symbols_over_mouth::face_with_symbols_over_mouth::face_with_symbols_over_mouth:')
+      message.delete()
+    } else {
+      buffer.push(message.content)
+    }
+  })
+}
+
+function setMessageFilter(channel) {
+  const filter = message => message.content.includes('discord') // TODO: create message filter
+  const collector = channel.createMessageCollector({ filter })
+  collector.on('collect', message => {
+    message.channel.send('VOCÊ NÃO PODE MANDAR ISSO SEU COISA')
+    message.delete()
+  })
+}
+
+client.once('ready', async () => {
+  console.log('Ready!')
+
+  const guild = await client.guilds.fetch(guildId)
+
+  setCommands(guild)
 
   guild.channels.cache.each(channel => {
     if (channel.type === 'GUILD_TEXT') {
-      const collector = channel.createMessageCollector({ filter })
+      setMessageFilter(channel)
 
-      collector.on('collect', message => {
-        message.channel.send('VOCÊ NÃO PODE MANDAR ISSO SEU COISA')
-        message.delete()
-      })
-
-      const spamCollector = channel.createMessageCollector({ filter: message => !message.author.bot })
-      spamCollector.on('collect', message => {
-        if (buffer.length >= 1000) {
-          buffer.pop()
-        }
-        if (buffer.includes(message.content)) {
-          message.channel.send('PARA DE SPAMMAR OW :face_with_symbols_over_mouth::face_with_symbols_over_mouth::face_with_symbols_over_mouth::face_with_symbols_over_mouth:')
-          message.delete()
-        } else {
-          buffer.push(message.content)
-        }
-      })
+      setSpamCollector(channel)
     }
   })
 })
